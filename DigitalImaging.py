a4b091587e07c17d7c3230e027d197186cc9b79a
ShayGali <shayali100@gmail.com>
2022-07-11 14:35:27 +0300

    implement make_collage function

--- DigitalImaging.py
+++ DigitalImaging.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 import dataclasses as dc
+from typing import List
+
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as pyplot
@@ -7,7 +9,6 @@ import matplotlib.pyplot as pyplot
 
 @dataclass
 class DigitalImaging:
-
     def convert_to_gs(self, img_path: str):
         pass
 
@@ -25,21 +26,37 @@ class DigitalImaging:
         if RGB_char not in valid_chars:
             raise ValueError(f"RGB_char need to be ('R', 'G', 'B'), you pass {RGB_char}")
 
-        img_arr = np.array(np.array(Image.open(path)))
+        img_arr = np.array(Image.open(path))
 
         img_one_color_channel = img_arr.copy()
 
-        if RGB_char == 'R':
-            img_one_color_channel[:, :, (1, 2)] = 0 # will take all the pixels and in channels 1 and 2 (green and blue), put the value 0
-        elif RGB_char == 'G':
-            img_one_color_channel[:, :, (0, 2)] = 0 # will take all the pixels and in channels 0 and 2 (red and blue), put the value 0
-        elif RGB_char == 'B':
-            img_one_color_channel[:, :, (0, 1)] = 0 # will take all the pixels and in channels 0 and 1 (red and green), put the value 0
+        if RGB_char == 'R':  # will take all the pixels and in channels 1 and 2 (green and blue), put the value 0
+            img_one_color_channel[:, :, (1, 2)] = 0
+        elif RGB_char == 'G':  # will take all the pixels and in channels 0 and 2 (red and blue), put the value 0
+            img_one_color_channel[:, :, (0, 2)] = 0
+        elif RGB_char == 'B':  # will take all the pixels and in channels 0 and 1 (red and green), put the value 0
+            img_one_color_channel[:, :, (0, 1)] = 0
 
-        return Image.fromarray(img_one_color_channel, 'RGB') # convert the array to image
+        return Image.fromarray(img_one_color_channel, 'RGB')  # convert the array to image
 
-    def make_collage(self):
-        pass
+    def make_collage(self, images_list: List) -> np.ndarray:
+
+        if any([not isinstance(img, Image.Image) for img in images_list]):
+            raise ValueError("all images need to be of type PIL.Image")
+
+        images_list_as_array = [np.array(img) for img in images_list]  # convert all images to ndarray
+
+        new_images = []
+        for (index, img) in enumerate(images_list_as_array):
+            if (index // 3) % 3 == 0:
+                img[:, :, (1, 2)] = 0
+            elif (index // 3) % 3 == 1:
+                img[:, :, (0, 2)] = 0
+            else:
+                img[:, :, (0, 1)] = 0
+            new_images.append(img)
+
+        return np.vstack(new_images)
 
     def shapes_dict(self):
         pass
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from DigitalImaging import DigitalImaging
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as pyplot
 
 agriculture_img_path = "./assets/agriculture.jpg"
+dog_folder_path = "./assets/dogs/"
 d = DigitalImaging()
 
 # Test reduce_to function
-img_red = d.reduce_to(agriculture_img_path, 'R')
-img_green = d.reduce_to(agriculture_img_path, 'G')
-img_blue = d.reduce_to(agriculture_img_path, 'B')
-img_red.show()
-img_green.show()
-img_blue.show()
+# img_red = d.reduce_to(agriculture_img_path, 'R')
+# img_green = d.reduce_to(agriculture_img_path, 'G')
+# img_blue = d.reduce_to(agriculture_img_path, 'B')
+# img_red.show()
+# img_green.show()
+# img_blue.show()
+
+
+# Test for make_collage function
+Image.open(agriculture_img_path)
+images = [
+    Image.open(f'{dog_folder_path}bulldog.jpg'), Image.open(f'{dog_folder_path}cocker-spaniel.jpg'),
+    Image.open(f'{dog_folder_path}continental-bulldog.jpg'), Image.open(f'{dog_folder_path}corgi.jpg'),
+    Image.open(f'{dog_folder_path}dog.jpg'), Image.open(f'{dog_folder_path}dog.jpg'),
+    Image.open(f'{dog_folder_path}dog3.jpg'),Image.open(f'{dog_folder_path}dog4.jpg'),
+    Image.open(f'{dog_folder_path}maltese.jpg'), Image.open(f'{dog_folder_path}puppy.jpg'),
+    Image.open(f'{dog_folder_path}puppy2.jpg'), Image.open(f'{dog_folder_path}puppy3.jpg'),
+
+]
+
+dogs_collage = d.make_collage(images)
+# print(dogs_collage)
+
+Image.fromarray(dogs_collage, 'RGB').show()
